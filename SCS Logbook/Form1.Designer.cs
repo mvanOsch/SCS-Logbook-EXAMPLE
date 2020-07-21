8dc9f19d53f88b9f0ac7cf10071245c0507c8842
Thomas tsgkn <niriaki@live.com>
2020-07-21 15:11:40 +0300

    Initial Release

---
+++ SCS Logbook/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿﻿namespace SCS_Logbook
+{
+    partial class LogBook
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LogBook));
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.LogPerTrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.SaveLog = new System.Windows.Forms.ToolStripMenuItem();
+            this.printLogToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.CopyLog = new System.Windows.Forms.ToolStripMenuItem();
+            this.Txt_Log = new System.Windows.Forms.RichTextBox();
+            this.statusStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 478);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(768, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {this.menuToolStripMenuItem, this.editToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(768, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {this.settingsToolStripMenuItem, this.toolStripSeparator1, this.SaveLog, this.printLogToolStripMenuItem, this.toolStripSeparator2});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {this.LogPerTrip});
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(171, 22);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // LogPerTrip
+            // 
+            this.LogPerTrip.CheckOnClick = true;
+            this.LogPerTrip.Name = "LogPerTrip";
+            this.LogPerTrip.Size = new System.Drawing.Size(158, 22);
+            this.LogPerTrip.Text = "Use Log per Trip";
+            this.LogPerTrip.Click += new System.EventHandler(this.LogPerTrip_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(168, 6);
+            // 
+            // SaveLog
+            // 
+            this.SaveLog.Name = "SaveLog";
+            this.SaveLog.ShortcutKeys = ((System.Windows.Forms.Keys) ((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.SaveLog.Size = new System.Drawing.Size(171, 22);
+            this.SaveLog.Text = "Export Log";
+            this.SaveLog.Click += new System.EventHandler(this.SaveLog_Click);
+            // 
+            // printLogToolStripMenuItem
+            // 
+            this.printLogToolStripMenuItem.Name = "printLogToolStripMenuItem";
+            this.printLogToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys) ((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.P)));
+            this.printLogToolStripMenuItem.Size = new System.Drawing.Size(171, 22);
+            this.printLogToolStripMenuItem.Text = "Print Log";
+            this.printLogToolStripMenuItem.Click += new System.EventHandler(this.printLogToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(168, 6);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {this.CopyLog});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // CopyLog
+            // 
+            this.CopyLog.Name = "CopyLog";
+            this.CopyLog.ShortcutKeys = ((System.Windows.Forms.Keys) ((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.C)));
+            this.CopyLog.Size = new System.Drawing.Size(164, 22);
+            this.CopyLog.Text = "Copy log";
+            this.CopyLog.Click += new System.EventHandler(this.CopyLog_click);
+            // 
+            // Txt_Log
+            // 
+            this.Txt_Log.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Txt_Log.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Txt_Log.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Txt_Log.Enabled = false;
+            this.Txt_Log.Location = new System.Drawing.Point(0, 24);
+            this.Txt_Log.Name = "Txt_Log";
+            this.Txt_Log.ReadOnly = true;
+            this.Txt_Log.Size = new System.Drawing.Size(768, 454);
+            this.Txt_Log.TabIndex = 2;
+            this.Txt_Log.TabStop = false;
+            this.Txt_Log.Text = "";
+            // 
+            // LogBook
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(768, 500);
+            this.Controls.Add(this.Txt_Log);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon) (resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "LogBook";
+            this.Text = "SCS Logbook";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.ToolStripMenuItem CopyLog;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem LogPerTrip;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem printLogToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem SaveLog;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.RichTextBox Txt_Log;
+
+        #endregion
+    }
+}
+
---
+++ SCS Logbook/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Drawing;
+using System.Globalization;
+using System.Drawing.Printing;
+using System.IO;
+using System.Windows.Forms;
+using SCSSdkClient;
+using SCSSdkClient.Object;
+
+namespace SCS_Logbook
+{
+    #region Components legent
+    /// <summary>
+    /// ************************** COMPONENTS **************************
+    /// Txt_Log ==> RichText Box;
+    /// menuStrip1 ==> menu holder (Top - Header)
+    /// toolStripStatusLabel1 ==> Status bar (bottom)
+    /// ***************************** MENU *****************************
+    /// menuToolStripMenuItem ==> Header "Menu" button;
+    /// settingsToolStripMenuItem ==> Header "Menu -> Setting" Sub-menu button;
+    /// SaveLog ==> Header "Menu -> Export" Sub menu button;
+    /// printLogToolStripMenuItem ==> Header "Menu -> Print" Sub menu button;
+    /// ***************************** EDIT *****************************
+    /// editToolStripMenuItem ==> Header "Edit" button;
+    /// CopyLog ==> Header "Edit -> Copy Log" Sub menu button;
+    ///  </summary>
+    #endregion
+
+    public partial class LogBook : Form
+    {
+        // Telemetry SDK
+        private SCSSdkTelemetry Telemetry;
+        // Data coming form Telemetry Dll
+        private string Amount; // Final revenue from the Job
+        private ulong Income; // Initial revenue from the job
+        private int XP; // Experience points from the job 
+        private string Origin; // Jobs source city 
+        private string Destination; // Jobs destination city 
+        private string Corp_Origin; // Jobs source company 
+        private string Corp_Destination; // Jobs destination company 
+        // Printer
+        private readonly PrintDocument p_log = new PrintDocument();
+        private readonly PrintDialog p_dialog = new PrintDialog();
+
+        #region Telemetry Initialization
+        public LogBook()
+        {
+            InitializeComponent();
+            p_log.PrintPage += new PrintPageEventHandler(document_PrintPage);
+            Txt_Log.Text = "[" + DateTime.Now.ToString(CultureInfo.CurrentCulture.DateTimeFormat) + "] \nLogbook initialized";
+            //CheckForIllegalCrossThreadCalls = false; // Uncomment this line if you get CrossThread Calls Error while debug
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "Seeking game";
+            Telemetry = new SCSSdkTelemetry();
+
+            if (Telemetry.Error != null) return; // Break the code if SDK encounters an error
+            SubscribeData(); // If not Subscribe to the Listeners
+        }
+
+        private  void SubscribeData()
+        { 
+            //Automatically refresh values from SDK (1x/sec If sdk is not loaded (game is not running))
+            Telemetry.Data += Telemetry_Data;
+            Telemetry.JobCancelled += TelemetryJobCancelled;
+            Telemetry.JobDelivered += TelemetryJobDelivered;
+            Telemetry.JobStarted += TelemetryOnJobStarted;
+        }
+        #endregion
+        
+        //***************** Telemetry Functions *****************
+        #region Game Data
+        private void Telemetry_Data(SCSTelemetry data, bool updated)
+        {
+            // Use any of the available data provided by the SDK here.
+            // Use SCSTelemetry "data" parameter.
+            toolStripStatusLabel1.Text = "Game: " + data.Game;
+
+            if (data.SpecialEventsValues.JobDelivered)
+            {
+                // Sets the Revenue and the XP when "JobDelivered" event occured
+                Amount = data.GamePlay.JobDelivered.Revenue.ToString("#0");
+                XP = data.GamePlay.JobDelivered.EarnedXp;
+            }
+
+            if (data.SpecialEventsValues.JobCancelled)
+            {
+                // Sets the Loss(money) "JobCancelled" event occured
+                Amount = data.GamePlay.JobCancelled.Penalty.ToString("#0");
+            }
+
+            if (data.SpecialEventsValues.OnJob)
+            {
+                // Sets useful information when you have active job (Used for "OnJobStarted" and "JobDelivered" events)
+                Origin = data.JobValues.CitySource;
+                Destination = data.JobValues.CityDestination;
+                Income = data.JobValues.Income;
+                Corp_Destination = data.JobValues.CompanyDestination;
+                Corp_Origin = data.JobValues.CompanySource;
+            }
+        }
+        #endregion
+
+        #region Game Events
+        private void TelemetryOnJobStarted(object sender, EventArgs e) // Action when the corresponding event occurs
+        { Txt_Log.Text += "\nJob Started OR loaded: " + Origin + " -> " + Destination + " [$" + Income + "]"; }
+
+        private void TelemetryJobCancelled(object sender, EventArgs e) // Action when the corresponding event occurs
+        { Txt_Log.Text += "\nJob Canceled: You paid $" + Amount + " in damages"; }
+
+        private void TelemetryJobDelivered(object sender, EventArgs e) // Action when the corresponding event occurs
+        { Txt_Log.Text += "\nJob from " + Corp_Origin + " delivered to " + Corp_Destination + ". \nYou have been paid $" + Amount + " and earned " + XP + "XP"; }
+
+        private void TelemetryFined(object sender, EventArgs e)
+        {
+            // Do stuff here when Fined
+            //Usage: Telemetry.Fined +=TelemetryFined;
+            //Should be on "SubscribeData()" Function
+        }
+
+        private void TelemetryTollgate(object sender, EventArgs e)
+        {
+            // Do stuff here when use Tollgates
+            //Usage: Telemetry.Tollgate +=TelemetryTollgate;
+            //Should be on "SubscribeData()" Function
+        }
+
+        private void TelemetryFerry(object sender, EventArgs e)
+        {
+            // Do stuff here when use Ferry
+            //Usage: Telemetry.Ferry +=TelemetryFerry;
+            //Should be on "SubscribeData()" Function
+        }
+
+        private void TelemetryTrain(object sender, EventArgs e)
+        {
+            // Do stuff here when use Train
+            //Usage: Telemetry.Train +=TelemetryTrain;
+            //Should be on "SubscribeData()" Function
+        }
+
+        private void TelemetryRefuel(object sender, EventArgs e)
+        {
+            // Do stuff here when Refuel. (Normally used for "Telemetry.RefuelPayed")
+            //Usage: Telemetry.RefuelPayed +=TelemetryRefuel;
+            //Should be on "SubscribeData()" Function
+        }
+        #endregion
+
+        //******************* Menu Functions ********************
+        #region Copy Log
+        private void CopyLog_click(object sender, EventArgs e)
+        {
+            if (Txt_Log.Text != string.Empty)
+                Clipboard.SetText(Txt_Log.Text);
+        }
+        #endregion
+
+        #region Print Log
+
+        private void document_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            e.Graphics.DrawString(Txt_Log.Text, new Font("Arial", 20, FontStyle.Regular), Brushes.Black, 20, 20);
+        }
+        private void printLogToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            p_dialog.Document = p_log;
+            if (p_dialog.ShowDialog() == DialogResult.OK)
+            {
+                p_log.Print();
+            }
+        }
+        #endregion
+
+        #region Save Log
+        private void SaveLog_Click(object sender, EventArgs e)
+        {
+            const string SavePath = "Logs/";
+            const string Extension = ".txt";
+            var Route = " (" + Origin + "-" + Destination + ") "; // For TripSave variable (cleaner)
+            // Vars for the different save system. You can use these directly, but this way keeps code cleaner
+            var TripSave = SavePath + DateTime.Now.Date.ToString("MMM-dd-yy") + Route + Extension;
+            var UniSave = SavePath + DateTime.Now.Date.ToString("MMM-dd-yy") + Extension;
+            
+            if (!Directory.Exists(SavePath))// Create "Logs" folder in case is does not exist (deleted)
+                Directory.CreateDirectory(SavePath);
+
+            if (LogPerTrip.Checked)
+            {
+                // If LogPerTrip is enabled, The "JobDelivered" event will trigger this save function
+                // And clear the Log text when its done
+                File.WriteAllText(TripSave, Txt_Log.Text);
+                Txt_Log.Clear();
+            }
+            else
+            {
+                // This is the manual Save. You have to press the "Export" button (Or Ctrl+S Hotkey) 
+                File.WriteAllText(UniSave, Txt_Log.Text);
+            }
+        }
+
+        private void LogPerTrip_Click(object sender, EventArgs e)
+        {
+            if (LogPerTrip.Checked) // If LogPerTrip is enabled the "JobDelivered" event Triggers "SaveLog_Click" function
+                Telemetry.JobDelivered += SaveLog_Click;
+            else
+                Telemetry.JobDelivered -= SaveLog_Click; //Stop Listening When "LogPerTrip" is disabled
+
+        }
+        #endregion
+
+    }
+}
